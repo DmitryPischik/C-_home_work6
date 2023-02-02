7b6cb8d751eb7836a398c6836e916436f7082a3a
Dmitriy Pischik <pischik@bk.ru>
2023-02-02 20:46:11 +1000

    Folder Task3 is added

---
+++ Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+void NewArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i % 2 ==0 && j % 2 == 0) matr[i,j] = matr[i,j] * matr[i,j];
+            Console.Write($"{matr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+NewArray(array);
