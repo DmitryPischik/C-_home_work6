af3159ed34d7aa2bf6cb7f9bb7880da5045eb72d
Dmitriy Pischik <pischik@bk.ru>
2023-02-02 20:56:33 +1000

    Folder Task4 is added

---
+++ Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+void DiagonalSum(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        sum += matr[i,i];        
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[n, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+DiagonalSum(array);
