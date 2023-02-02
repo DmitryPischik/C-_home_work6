ed77ba043ca0ef0a622ad1dcbbd9ce6746fa6cba
Dmitriy Pischik <pischik@bk.ru>
2023-02-02 20:36:29 +1000

    Folder Task2 is added

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] matr) // функция просматривает массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr) // функция заполняет массив.
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i + j;
+        }
+    }
+}
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
